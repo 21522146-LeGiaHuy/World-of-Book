5057fc47fbb05db37a2cdc1892ed1fcd9149097f
Hoàng Gia Huy <134790884+HoangGiaHuy23@users.noreply.github.com>
2024-05-31 23:45:49 +0700

    Update style.js

--- src/components/CardComponent/style.js
+++ src/components/CardComponent/style.js
@@ -1,79 +1,33 @@
-import { Card } from "antd";
-
 import styled from "styled-components";
 
-export const MyCustomCardBody = styled(Card)`
-padding: 20px;
-background-color: #f0f0f0;
-`
-
-export const WrapperCardStyle = styled(Card)`
-    width: 200px;
-    padding: 0px;
-    & img {
-        height: 200px;
-        width: 200px;
-    }
-    & Body {
-        padding: 0;
-    }
-`
-
-export const ContentCenterFlex = styled.div`
+export const WrapperContent = styled.div`
     display: flex;
-    justify-content: center;
+    //align-items: center;
+    flex-direction: column;
+    gap: 12px;
+    margin: 10px 0 20px;
 `
-
-export const SpecialPrice = styled.div`
-    font-size: 1.35em;
-    color: #DC0019;
+export const WrapperLabelText = styled.span`
+    cursor: pointer;
+    text-transform: uppercase;
+    line-height: 100%;
+    padding: 10px 0;
+    text-align: left;
     font-weight: 600;
-`
-export const DiscountPercentBackground = styled.div`
-    background-color: #C92127;
-    min-width: 40px;
-    height: 22px;
-    border-radius: 5px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-top: 2px !important;
-    margin-left: 5px !important;
+    margin-bottom: 0px;
+    font-size: 13px;
 `
 
-export const DiscountPercent = styled.div`
-    color: #ffffff;
-    font-size: 11px !important;
+export const WrapperTextValue = styled.span`
+    color: #666;
+    padding: 0 0 0 10px;
+    display: inline;
+    zoom: 1;
+    font-size: 14px;
+    text-transform: capitalize;
+`
+export const WrapperSelectedTextValue = styled.span`
+    color: #F7941E;
     font-weight: bold;
-`
-
-export const OldPrice = styled.div`
-    display: flex;
-    justify-content: center;
-    color: #888888;
-    text-decoration: line-through;
-    font-size: 1.1em;
-`
-
-export const Sold = styled.div`
-    display: flex;
-    justify-content: center;
-    color: #5a5a5a;
-    font-size: 1.15em;
-`
-
-export const StyleNameProduct = styled.div`
-    padding-top: 0px !important;
-    line-height: 1.4em;
-    word-break: break-word;
-    white-space: normal;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    display: -webkit-box;
-    -webkit-line-clamp: 2;
-    -webkit-box-orient: vertical;
-    min-height: 2.8em;
-    max-height: 2.8em;
-    font-size: 1em;
-    color: #333333;
+    font-size: 14px;
 `
