a4b27f62451dd49ad62298003eb7b4d01faf7e7a
Hoàng Gia Huy <134790884+HoangGiaHuy23@users.noreply.github.com>
2024-05-31 23:45:33 +0700

    Update CardComponent.jsx

--- src/components/CardComponent/CardComponent.jsx
+++ src/components/CardComponent/CardComponent.jsx
@@ -1,19 +1,22 @@
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-
-function CardComponent() {
+import React from 'react';
+import { Card } from 'antd';
+import { ProductName, Discount, SpecialPrice, Price, OldPrice } from './style';
+function CardComponent(){
   return (
-    <Card style={{ width: '240px', height: '340px' }}>
-      <Card.Img style={{height: '190px'}} variant="top" src="https://cdn0.fahasa.com/media/catalog/product/i/m/image_183396.jpg" />
-      <Card.Body style={{border: 3}}>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text style={{padding: 40}}>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
+    <Card
+    className='bodyPadding'
+    hoverable
+    bordered
+    style={{ width: 220, alignItems : 'center', display : 'flex', flexDirection : 'column'}}
+    cover={<img alt="example" style={{ width : 190, height : 160}} src="https://cdn0.fahasa.com/media/catalog/product/i/m/image_183396.jpg" />}
+  >
+    <ProductName>Nghệ Thuật Tư Duy Chiến Lược</ProductName>
+    <Price>
+      <SpecialPrice>155.350 đ</SpecialPrice>
+      <Discount>35%</Discount>
+    </Price>
+    <Price><OldPrice>239.000 đ</OldPrice></Price> 
+  </Card>
   );
 }
 
